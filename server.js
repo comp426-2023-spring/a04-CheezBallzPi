376d57ffcf93ed1244aaf7a5c52126c5f27764fe
Jackie Chen <cheezballz3.14@gmail.com>
2023-03-20 16:30:41 -0400

    Finish all api calls

--- server.js
+++ server.js
@@ -1,44 +1,45 @@
-import { rps, rpsls } from "../lib/rpsls.js";
-import { express } from "express";
-
-const [,, ...args] = process.argv;
+import { rps, rpsls } from "./lib/rpsls.js";
+import express from "express";
+import parseArgs from "minimist";
 
 const app = express()
 let hostname = "128.0.0.1" // localhost
-let port = 5000;
 
 // Allow parsing of keys in API call
-app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }))
 
 // Parse args
-for(let i = 0; i < args.length; i++) {
-    switch(args[i]) {
-        case "--port":
-            port = parseInt(args[i + 1]);
-    }
-}
+var args = parseArgs(process.argv.slice(2), {string: 'port'});
+
+var port = ('port' in args) ? parseInt(args.port) : 5000;
 
 app.get('/app/', (req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/html',
-    }).end("OK");
+    res.set('Content-Type', 'text/html')
+    res.status(200).send("OK")
 })
 
 app.get('/app/rps', (req, res) => {
+    res.set('Content-Type', 'text/html')
     res.send(JSON.stringify(rps()))
 })
 
 app.get('/app/rpsls', (req, res) => {
+    res.set('Content-Type', 'text/html')
     res.send(JSON.stringify(rpsls()))
 })
 
 app.get('/app/rps/play', (req, res) => {
+    res.set('Content-Type', 'text/html')
     res.send(JSON.stringify(rps(req.body.shot)))
 })
 
 app.get('/app/rpsls/play', (req, res) => {
+    res.set('Content-Type', 'text/html')
     res.send(JSON.stringify(rpsls(req.body.shot)))
+})
+
+app.use((req, res, next) => {
+    res.status(404).send("NOT FOUND")
 })
 
 // Create the server
