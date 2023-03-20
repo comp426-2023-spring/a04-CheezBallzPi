e94f311fa6d1c655bca577146962c995b3c2c225
Jackie Chen <cheezballz3.14@gmail.com>
2023-03-20 16:38:46 -0400

    Fix some specifics

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 let hostname = "128.0.0.1" // localhost
 
 // Allow parsing of keys in API call
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 // Parse args
 var args = parseArgs(process.argv.slice(2), {string: 'port'});
@@ -15,7 +15,7 @@ var port = ('port' in args) ? parseInt(args.port) : 5000;
 
 app.get('/app/', (req, res) => {
     res.set('Content-Type', 'text/html')
-    res.status(200).send("OK")
+    res.status(200).send("200 OK")
 })
 
 app.get('/app/rps', (req, res) => {
@@ -23,12 +23,12 @@ app.get('/app/rps', (req, res) => {
     res.send(JSON.stringify(rps()))
 })
 
-app.get('/app/rpsls', (req, res) => {
+app.post('/app/rpsls', (req, res) => {
     res.set('Content-Type', 'text/html')
     res.send(JSON.stringify(rpsls()))
 })
 
-app.get('/app/rps/play', (req, res) => {
+app.post('/app/rps/play', (req, res) => {
     res.set('Content-Type', 'text/html')
     res.send(JSON.stringify(rps(req.body.shot)))
 })
@@ -38,8 +38,18 @@ app.get('/app/rpsls/play', (req, res) => {
     res.send(JSON.stringify(rpsls(req.body.shot)))
 })
 
+app.get('/app/rps/play:shot', (req, res) => {
+    res.set('Content-Type', 'text/html')
+    res.send(JSON.stringify(rps(req.params.shot)))
+})
+
+app.get('/app/rpsls/play:shot', (req, res) => {
+    res.set('Content-Type', 'text/html')
+    res.send(JSON.stringify(rpsls(req.params.shot)))
+})
+
 app.use((req, res, next) => {
-    res.status(404).send("NOT FOUND")
+    res.status(404).send("404 NOT FOUND")
 })
 
 // Create the server
